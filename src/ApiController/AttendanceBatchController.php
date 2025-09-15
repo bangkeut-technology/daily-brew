8882f991e18015fb315e78e68122d46c83d3215c
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2025-09-15 14:49:00 +0700

    feat(attendance): add AttendanceBatch form and controller
    - Introduced `AttendanceBatchFormType` for form management.
    - Added `AttendanceBatchController` as a new API controller.

---
+++ src/ApiController/AttendanceBatchController.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ApiController;
+
+use App\Controller\AbstractController;
+
+/**
+ * Class AttendanceBatchController
+ *
+ * @package App\ApiController
+ * @author  Vandeth THO <thovandeth@gmail.com>
+ */
+class AttendanceBatchController extends AbstractController
+{
+
+}
---
+++ src/Form/AttendanceBatchFormType.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Form;
+
+use App\Entity\AttendanceBatch;
+use App\Entity\User;
+use App\Enum\AttendanceBatchTypeEnum;
+use App\Form\Type\DateTimeImmutableType;
+use App\Form\Type\DateTimeType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class AttendanceBatchFormType
+ *
+ * @package App\Form
+ * @author  Vandeth THO <thovandeth@gmail.com>
+ */
+class AttendanceBatchFormType extends AbstractFormType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type', EnumType::class, [
+                'class' => AttendanceBatchTypeEnum::class,
+            ])
+            ->add('label', TextType::class)
+            ->add('note', TextType::class)
+            ->add('fromDate', DateTimeImmutableType::class)
+            ->add('toDate', DateTimeImmutableType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AttendanceBatch::class,
+        ]);
+    }
+}
