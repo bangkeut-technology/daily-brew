fbc8ea5bd7521c48b45a8efe704f63ebfff21ef4
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2025-09-16 07:29:43 +0700

    feat(attendance): add dependencies and constructor to AttendanceBatchController
    - Injected `TranslatorInterface` and `AttendanceBatchRepository` into the controller.
    - Added a constructor to initialize the dependencies.
    - Created an empty `post` method for future implementation.

--- src/ApiController/AttendanceBatchController.php
+++ src/ApiController/AttendanceBatchController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace App\ApiController;
 
 use App\Controller\AbstractController;
+use App\Repository\AttendanceBatchRepository;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class AttendanceBatchController
@@ -13,5 +15,16 @@ use App\Controller\AbstractController;
  */
 class AttendanceBatchController extends AbstractController
 {
+    public function __construct(
+        TranslatorInterface $translator,
+        AttendanceBatchRepository $attendanceBatchRepository,
+    )
+    {
+        parent::__construct($translator);
+    }
 
+    public function post()
+    {
+        
+    }
 }
